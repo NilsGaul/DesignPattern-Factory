0c7df328cc3c76ca24801fa0ae6e95b901436b40
Nils <nilsgaul@web.de>
2024-04-21 12:51:03 +0200

    C#: setup the factory design pattern

---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("This is an Factory Design Pattern.");
+Console.WriteLine("A Factory design pattern is a creational pattern.");
